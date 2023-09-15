5501d65136cce084c626fec75ecbe8f6582481bd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 09:37:47 -0300

    adicionando nosso primeiro javasript

---
+++ aula.js
@@ -0,0 +1,17 @@
+// Aqui é um alert
+alert('ola mundo!!');
+
+/*
+Aprendendo javasript.
+Muito legal!
+*/
+
+//imprimindo mensagem na tela
+document.write('Meu nome é Enzo');
+
+//usando console
+console.log('olá tudo bem?');
+
+//Criando nossa primeira variavel
+let mensagem = "ola mundo";
+console.log(mensagem);
