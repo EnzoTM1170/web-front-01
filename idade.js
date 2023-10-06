15269b2f6078ce627573821a377f8ca75727ba42
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-06 09:12:02 -0300

    adicionando arquivo

---
+++ idade.js
@@ -0,0 +1,11 @@
+let idade = 14;
+
+if(idade <= 12) {
+    console.log("É uma criança");
+} else if (idade <= 17){
+    console.log("É um adolescente");
+}else if (idade >= 18){
+    console.log("É um adulto");
+}else if (idade >= 50){
+    console.log("É um velho");
+}
