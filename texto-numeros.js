71342fbaa289674ae9c76395433b2ef7e40fefa7
EnzoTM1170 <143804193+EnzoTM1170@users.noreply.github.com>
2023-11-17 09:43:45 -0300

    adicioando novo are

--- texto-numeros.js
+++
@@ -1,5 +0,0 @@
-let numFloat = 123.5422;
-
-let numString = "123.5432";
-
-console.log(parseFloat (numString));
--- variavel.js
+++
@@ -1,9 +0,0 @@
-// cria uma variavel com valor indefinido
-
-
-let nome;
-//cria uma variavel vazia]
-let disciplina = null;
-let texto = 'este é um exemplo'
-
-console.log(texto)
